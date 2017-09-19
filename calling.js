3145234d9b674b12e76108c13d696a1891f87f3d
kiquetal <kiquetal@gmail.com>
2017-09-19 16:29:50 -0400

    new comparing

--- calling.js
+++ calling.js
@@ -4,7 +4,12 @@ var request=require('request');
 var msisdn=require('./msisdn.json');
 var arrayMsisdn=msisdn;
 var compareLib=require('./compareLib');
+var fs=require('fs');
 
+fs.writeFile("./write_file.xlsx","Msisdn\t\Diffs Fields\tValuesFields\n",function(err)
+{
+ console.log("cabeceras creadas");
+});
 
 async.each(arrayMsisdn,function(item,next)
  {
@@ -62,9 +67,54 @@ function callBSS(msisdn,arg1,callback)
                        
                        
                        var bssValues=compareLib.obtainArrayCritical(JSON.parse(body));
+                      /*  var headers=fs.createFile("./write_file.xlsx","msisdn\tfields\tValues",function(error)
+						{
+                          console.log("saved headers");
+						});
+*/
+						var contents = fs.appendFile("./write_file.xlsx", generateRowFile(msisdn,legacyValues,bssValues),
+ 						function(error)	{
+					
+									if(error) console.log("error")
+   									console.log("information save for: "+ msisdn);
+									callback(null,'MSISDN: '+msisdn +"\n"+" BSS VALUES: "+ JSON.stringify(bssValues) + "\n LEGACY VALUES: " +JSON.stringify(legacyValues));
+  										}
+ 									);
+	
+	
                       //  console.log(bssValues);
                       //  console.log(legacyValues);
-                        callback(null,'MSISDN: '+msisdn +"\n"+" BSS VALUES: "+ JSON.stringify(bssValues) + "\n LEGACY VALUES: " +JSON.stringify(legacyValues));
+                   //     callback(null,'MSISDN: '+msisdn +"\n"+" BSS VALUES: "+ JSON.stringify(bssValues) + "\n LEGACY VALUES: " +JSON.stringify(legacyValues));
                  }
              });
 }
+
+function generateRowFile(msisdn,dataLegacy,dataBss)
+{
+ 
+  var arrayNameFieldsDiff=[];
+  var arrayObjectDifferentsValues=[];
+  for (var el in dataLegacy)
+{
+  var valueLegacy=dataLegacy[el],
+	  valueBSS=dataBss[el];
+  var key=Object.keys(valueLegacy)[0];
+		
+    if (valueLegacy[key] != valueBSS[key])
+  {
+     arrayNameFieldsDiff.push(key);
+	 var obj={};
+      obj[key]={"bss":valueBSS[key],"legacy":valueLegacy[key]}
+     arrayObjectDifferentsValues.push(obj);
+      }
+  
+  else
+   {
+   console.log("equals values "+key);
+   }
+}
+ console.log(msisdn); 
+return msisdn+"\t"+arrayNameFieldsDiff.join("-")+"\t"+JSON.stringify(arrayObjectDifferentsValues)+"\n";
+
+}
+
