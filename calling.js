4b2112613ab1182888ef229a264ea6b0cb714e1f
kiquetal <kiquetal@gmail.com>
2017-12-28 15:24:14 -0300

    new files

--- calling.js
+++ calling.js
@@ -1,12 +1,13 @@
 var async=require('async');
 var config=require("./config.json");
 var request=require('request');
-var msisdn=require('./msisdn.json');
+var msisdn=require('./msisdn1_1.json');
 var arrayMsisdn=msisdn;
 var compareLib=require('./compareLib');
 var fs=require('fs');
 
-fs.writeFile("./write_file.xlsx","Msisdn\t\Diffs Fields\tValuesFields\n",function(err)
+//fs.writeFile("./write_file.xls","Msisdn\t\Diffs Fields\tValuesFields\n",function(err)
+fs.writeFile("./write_file.xls","Msisdn"+"\t"+"Values"+"\t"+"PlanType"+"\n",function(err)
 {
  console.log("cabeceras creadas");
 });
@@ -56,9 +57,11 @@ function callLegacy(opt,callback)
 }
 function callBSS(msisdn,arg1,callback)
 {
-    request({url:'https://'+config['hostQA']+config['pathAccount']+'/'+msisdn+'/account'},
+    request({url:'https://'+config['hostQA']+config['pathAccount']+'/'+msisdn+'/account',
+			headers:{'Authorization':'Bearer '+config['tokenBSS']}},
              function(error,response,body)
             {
+                 if (error) console.log("[[[[[[[[[]]]]]]]]]]]]"+JSON.stringify(error));
                  if (error) return callback(error);
                  else{
                     // console.log("no error");
@@ -72,7 +75,7 @@ function callBSS(msisdn,arg1,callback)
                           console.log("saved headers");
 						});
 */
-						var contents = fs.appendFile("./write_file.xlsx", generateRowFile(msisdn,legacyValues,bssValues),
+						var contents = fs.appendFile("./write_file.xls", generateRowFile(msisdn,legacyValues,bssValues),
  						function(error)	{
 					
 									if(error) console.log("error")
@@ -91,7 +94,7 @@ function callBSS(msisdn,arg1,callback)
 
 function generateRowFile(msisdn,dataLegacy,dataBss)
 {
- 
+  var planTypeLegacy=null;
   var arrayNameFieldsDiff=[];
   var arrayObjectDifferentsValues=[];
   for (var el in dataLegacy)
@@ -99,22 +102,22 @@ function generateRowFile(msisdn,dataLegacy,dataBss)
   var valueLegacy=dataLegacy[el],
 	  valueBSS=dataBss[el];
   var key=Object.keys(valueLegacy)[0];
-		
+  planTypeLegacy=valueLegacy["account.plan.planType"];		
     if (valueLegacy[key] != valueBSS[key])
   {
      arrayNameFieldsDiff.push(key);
 	 var obj={};
-      obj[key]={"bss":valueBSS[key],"legacy":valueLegacy[key]}
+      obj[key]={"bss":valueBSS[key]==null?"no-data-bss":valueBSS[key],"legacy":valueLegacy[key]}
      arrayObjectDifferentsValues.push(obj);
-      }
-  
+      
+ }
   else
    {
    console.log("equals values "+key);
    }
 }
  console.log(msisdn); 
-return msisdn+"\t"+arrayNameFieldsDiff.join("-")+"\t"+JSON.stringify(arrayObjectDifferentsValues)+"\n";
-
+//return msisdn+"\t"+arrayNameFieldsDiff.join("-")+"\t"+JSON.stringify(arrayObjectDifferentsValues)+"\n";
+ return msisdn+"\t"+JSON.stringify(arrayObjectDifferentsValues)+"\t"+ planTypeLegacy + "\n";
 }
 
